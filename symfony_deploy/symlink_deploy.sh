62df860c844f50f6211d18b50152beebf43f73d8
Daniil Osipov <dan4ex@yandex.ru>
2020-10-11 18:26:04 +0300

    Remove sudo command

--- symfony_deploy/symlink_deploy.sh
+++ symfony_deploy/symlink_deploy.sh
@@ -13,8 +13,8 @@ fi
 
 #Install package
 cd ~/
-sudo apt update
-sudo apt install -y wget nginx php7.2-cli php7.2-common php7.2-fpm php7.2-cgi php7.2-sqlite php7.2-mbstring php7.2-zip php7.2-xml
+apt update
+apt install -y wget nginx php7.2-cli php7.2-common php7.2-fpm php7.2-cgi php7.2-sqlite php7.2-mbstring php7.2-zip php7.2-xml
 
 #Install Symphony
 if [[ $(ls -la /usr/bin | grep symfony) ]]
@@ -34,24 +34,24 @@ else
   php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
   php composer-setup.php
   php -r "unlink('composer-setup.php');"
-  sudo mv composer.phar /usr/bin/composer
+  mv composer.phar /usr/bin/composer
 fi
 
 #Symfony create project
-sudo mkdir -p /var/www/$RELEASE && cd /var/www/$RELEASE
-sudo composer create-project $INSTALL_PROJECT $PROJECT_NAME
+mkdir -p /var/www/$RELEASE && cd /var/www/$RELEASE
+composer create-project $INSTALL_PROJECT $PROJECT_NAME
 
 #Configure nginx
 if [[ $(find /etc/nginx/sites-available/ -type f -name "default") ]]
 then
-  sudo rm -f /etc/nginx/sites-available/default
-  sudo rm -f /etc/nginx/sites-enabled/default
+  rm -f /etc/nginx/sites-available/default
+  rm -f /etc/nginx/sites-enabled/default
 fi
 if [[ $(find /etc/nginx/sites-available/ -type f -name $PROJECT_NAME) ]]
 then
   echo "nginx is already configure"
 else
-  sudo tee /etc/nginx/sites-available/$PROJECT_NAME <<EOF
+  tee /etc/nginx/sites-available/$PROJECT_NAME <<EOF
 server {
           listen 80 default_server;
           listen [::]:80 default_server;
@@ -76,8 +76,8 @@ server {
           }
   }
 EOF
-  sudo ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled/$PROJECT_NAME
-  sudo systemctl restart nginx
+  ln -s /etc/nginx/sites-available/$PROJECT_NAME /etc/nginx/sites-enabled/$PROJECT_NAME
+  systemctl restart nginx
 fi
 
 if [[ $? -eq 0 ]]
@@ -94,9 +94,9 @@ fi
 #Change owner
 echo "##############################"
 echo "CHANGE OWNER TO SITE DIRECTORY"
-sudo chown -R www-data:www-data /var/www/$RELEASE/$PROJECT_NAME
+chown -R www-data:www-data /var/www/$RELEASE/$PROJECT_NAME
 
 #Symlink deployment
 echo "##########################"
 echo "CREATE SYMLINK DEPLOYMENT"
-sudo ln -sfn /var/www/$RELEASE/$PROJECT_NAME/public /var/www/$SYMLINK
+ln -sfn /var/www/$RELEASE/$PROJECT_NAME/public /var/www/$SYMLINK
